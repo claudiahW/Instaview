ecd84befc2dc6d91a08d90c934ede78e10386de9
claudiahW <murungiclaud@gmail.com>
2021-12-03 16:00:29 +0300

    add comments class

--- instaview/models.py
+++ instaview/models.py
@@ -10,7 +10,9 @@ class Images(models.Model):
     title = models.CharField(max_length=100)
     caption= models.TextField(max_length=100)
     post_date = models.DateTimeField(auto_now_add=True,null=True)
-
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
     def save_image(self):
         self.save()
 
@@ -21,12 +23,12 @@ class Images(models.Model):
             self.delete()    
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     profile_photo = CloudinaryField('image')
     bio = models.TextField(max_length=500, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=True, null=True)
-
-    def update(self):
+    
+    def update(self,title,caption):
         self.save()
 
     def save_profile(self):
@@ -34,3 +36,22 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+
+    
+class Likes(models.Model):
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.likes
+
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=50)
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+    def save_comment(self):
+        self.save()
