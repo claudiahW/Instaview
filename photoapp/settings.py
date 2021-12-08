acf9f772c4c281ef14dd1c15ea0dc53a880efa79
claudiahW <murungiclaud@gmail.com>
2021-12-08 15:33:17 +0300

    edit host

--- photoapp/settings.py
+++ photoapp/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 cloudinary.config(
     cloud_name = config('CD_NAME'),
